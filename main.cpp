43a53d06046e711b20e7571608f4e7607cb25fbd
BaDunky <arturitosilva20@hotmail.com>
2019-09-28 13:20:19 -0700

    updated dollarsign

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cstring>
-
+#include <sstream>
 
 int main(){
   std::string location;
@@ -12,10 +12,10 @@ int main(){
   double meal;
   double total;
 
-  std::setprecision(2); // set precision after decimal
+  std::stringstream sshotel, ssmeal, sstotal;
 
   std::cout << "Welcome to the Business Trip Tracker!\n\nWhat is the business trip location? ";
-  getline(std::cin, location); //INPUT
+  getline(std::cin, location); //INPUT string with spaces
   std::cout << "How many days will the trip take? ";
   std::cin >> days; //INPUT
   std::cout << "What is the total hotel expense? $";
@@ -26,8 +26,12 @@ int main(){
 
   total = hotel + meal; // Calculates the total cost
 
+  sshotel << std::setprecision(2) << std::fixed << '$' << hotel;
+  ssmeal << std::setprecision(2) << std::fixed << '$' << meal;
+  sstotal << std::setprecision(2) << std::fixed << '$' << total;
+
   std::cout << std::setw(10) << "Location" << std::setw(10) << "Days" << std::setw(10) << "Hotel" << std::setw(10) << "Meal" << std::setw(10) << "Total\n";
-  std::cout << std::setw(10) << location << std::setw(10) << days << std::setw(10) << hotel << std::setw(10) << "$" + std::to_string(meal) << std::setw(10) << "$" + std::to_string(total) << '\n'; // Outputs in the desired format
+  std::cout << std::setw(10) << location << std::setw(10) << days << std::setw(10) << sshotel.str() << std::setw(10) << ssmeal.str() << std::setw(10) << sstotal.str() << '\n'; // Outputs in the desired format
 
   return 0;
 }
