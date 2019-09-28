e7ceda674cb43e3d8d9547b6ed00c4f1cfd824da
Arturo Silva <arturitosilva20@hotmail.com>
2019-09-28 11:05:17 -0700

    Added multiple location words

--- main.cpp
+++ main.cpp
@@ -12,20 +12,22 @@ int main(){
   double meal;
   double total;
 
+  std::setprecision(2); // set precision after decimal
+
   std::cout << "Welcome to the Business Trip Tracker!\n\nWhat is the business trip location? ";
-  std::cin >> location;
+  getline(std::cin, location); //INPUT
   std::cout << "How many days will the trip take? ";
-  std::cin >> days;
+  std::cin >> days; //INPUT
   std::cout << "What is the total hotel expense? $";
-  std::cin >> hotel;
+  std::cin >> hotel; //INPUT
   std::cout << "What is the total meal expense? $" ;
-  std::cin >> meal;
+  std::cin >> meal; //INPUT
   std::cout << '\n';
 
-  total = hotel + meal;
+  total = hotel + meal; // Calculates the total cost
 
   std::cout << std::setw(10) << "Location" << std::setw(10) << "Days" << std::setw(10) << "Hotel" << std::setw(10) << "Meal" << std::setw(10) << "Total\n";
-  std::cout << std::setw(10) << location << std::setw(10) << days << std::setw(10) << hotel << std::setw(10) << meal << std::setw(10) << total << '\n';
+  std::cout << std::setw(10) << location << std::setw(10) << days << std::setw(10) << hotel << std::setw(10) << "$" + std::to_string(meal) << std::setw(10) << "$" + std::to_string(total) << '\n'; // Outputs in the desired format
 
   return 0;
 }
